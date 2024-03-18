6cd6090f2c54f66e7956ec7e61118901ad7d7f39
dDionWiratama <dionwiratama04@gmail.com>
2024-03-18 12:14:54 +0700

    Update Tugas

--- app/src/main/java/com/example/aplikasikotlin/AquariumFish.kt
+++ app/src/main/java/com/example/aplikasikotlin/AquariumFish.kt
@@ -1,58 +1,28 @@
-package com.example.aplikasikotlin
+package id.ac.pnm.ti2a.aplikasi
 
-abstract class AquariumFish {
-    abstract val color: String
+interface FishAction {
+    fun eat()
+}
 
-    interface FishAction {
-        fun eat()
+interface FishColor {
+    val color: String
+}
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
     }
+}
 
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
 
-    class Shark : AquariumFish(), FishAction {
-        override val color = "grey"
-        override fun eat() {
-            println("hunt and eat fish")
-        }
-
-        interface FishColor {
-            val color: String
-        }
-
-        object GoldColor : FishColor {
-            override val color = "gold"
-        }
-
-        class PrintingFishAction(val food: String) : FishAction {
-            override fun eat() {
-                println(food)
-            }
-        }
-
-        class Plactomous : AquariumFish(), FishAction {
-            override val color = "gold"
-            override fun eat() {
-                println("eat algae")
-            }
-        }
-
-        interface FishAction {
-            fun eat()
-        }
-
-        class Plecostomus(fishColor: FishColor = GoldColor) :
-            FishAction by PrintingFishAction("eat algae"),
-            FishColor by fishColor
-
-        abstract class AquariumFish : FishAction {
-            abstract val color: String
-            override fun eat() = println("yum")
-            class Shark : FishAction, FishColor {
-                override val color = "grey"
-                override fun eat() {
-                    println("hunt and eat fish")
-                }
-
-            }
-        }
+class Shark: FishAction, FishColor {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
     }
 }
--- app/src/main/java/com/example/aplikasikotlin/decoration.kt
+++ app/src/main/java/com/example/aplikasikotlin/decoration.kt
@@ -1,9 +1,9 @@
-package com.example.aplikasikotlin
+package id.ac.pnm.ti2a.aplikasi
+
 
 data class Decoration(val rocks: String) {
 
 }
-
 data class Decoration2(val rocks: String, val wood: String, val diver: String){
 }
 fun makeDecorations() {
